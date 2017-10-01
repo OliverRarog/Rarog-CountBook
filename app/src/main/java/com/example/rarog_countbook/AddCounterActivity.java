98f59ed5d485b364ffd7ccb074894ad44ea5e466
OliverRarog <oliverrarog@gmail.com>
2017-09-30 20:21:53 -0600

    Now create counter through buttons, still can't view and edit, data preservation not implemented

--- app/src/main/java/com/example/rarog_countbook/AddCounterActivity.java
+++ app/src/main/java/com/example/rarog_countbook/AddCounterActivity.java
@@ -1,17 +1,94 @@
 package com.example.rarog_countbook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddCounterActivity extends AppCompatActivity {
+    Button createButton;
+    EditText counterNameEditText;
+    EditText counterCommentEditText;
+    EditText counterValueEditText;
+
+
+    /* idea to disable button until required editText fields filled
+    taken from
+    https://stackoverflow.com/questions/20682865/disable-button-when-edit-text-fields-empty
+    */
+    private TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            checkRequiredFields();
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {}
+    };
+
+    private void checkRequiredFields() {
+        String nameField = counterNameEditText.getText().toString();
+        String valueField = counterValueEditText.getText().toString();
+
+        if(verifyNameField(nameField) || verifyValueField(valueField) ) {
+            createButton.setEnabled(false);
+        }
+        else {
+            createButton.setEnabled(true);
+        }
+    }
+
+    private Boolean verifyNameField(String name) {
+        return name.trim().isEmpty();
+    }
+
+    private Boolean verifyValueField(String value) {
+        // checks that user typed in a non-negative number, no non-numeric characters allowed
+        return (Integer.getInteger(value) != null && Integer.getInteger(value) >= 0);
+    }
+
+    private void instantiateButtonsAndFields() {
+        createButton = (Button) findViewById(R.id.createCounterButton);
+        counterNameEditText = (EditText) findViewById(R.id.counterNameEditText);
+        counterCommentEditText = (EditText) findViewById(R.id.counterCommentEditText);
+        counterValueEditText = (EditText) findViewById(R.id.counterValueEditText);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_counter);
 
-        
+        instantiateButtonsAndFields();
 
+        counterNameEditText.addTextChangedListener(textWatcher);
+        counterValueEditText.addTextChangedListener(textWatcher);
 
+        createButton.setEnabled(false);
+
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nameText = counterNameEditText.getText().toString();
+                String commentText = counterCommentEditText.getText().toString();
+                int value = Integer.parseInt(counterValueEditText.getText().toString());
+
+                Intent intent = getIntent();
+                intent.putExtra("nameText", nameText);
+                intent.putExtra("commentText", commentText);
+                intent.putExtra("initialValue", value);
+
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/rarog_countbook/MainActivity.java
+++ app/src/main/java/com/example/rarog_countbook/MainActivity.java
@@ -6,10 +6,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int ADD_COUNTER_ACTIVITY = 1;
+    ArrayList<Counter> counterList = new ArrayList<Counter>();
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK && requestCode == ADD_COUNTER_ACTIVITY) {
+            String nameText = data.getExtras().getString("nameText");
+            String commentText = data.getExtras().getString("commentText");
+            int initialValue = data.getExtras().getInt("initialValue");
+
+            counterList.add(new Counter(initialValue, nameText, commentText));
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,18 +35,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent addCounterIntent = new Intent(view.getContext(), AddCounterActivity.class);
-                startActivity(addCounterIntent);
+                startActivityForResult(addCounterIntent, ADD_COUNTER_ACTIVITY);
             }
         });
 
-        ArrayList<Counter> list = new ArrayList<Counter>();
-        list.add(new Counter(5, "test1", "hello"));
-        list.add(new Counter(10, "test2", "bye"));
-        for(int i = 0; i < 15; i++) {
-            list.add(new Counter(i, "test", "test"));
-        }
 
-        CounterRowAdapter adapter = new CounterRowAdapter(list, this);
+        CounterRowAdapter adapter = new CounterRowAdapter(counterList, this);
 
         ListView lView = (ListView) findViewById(R.id.counterListView);
         lView.setAdapter(adapter);
